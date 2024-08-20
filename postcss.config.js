286355448d2189023257223d0324eb825cf08de7
FC57 <franky_57@163.com>
2024-08-20 12:13:11 +0800

    postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: {
     'postcss-preset-env': {
       stage: 0, // 哪怕是草案阶段的语法，也需要转换
-      preserve: false
+      preserve: false // 编译后，新语法不可见，比如 color:var(--color) 编译后不可见，仅展示 color:颜色
     }
   }
 };
