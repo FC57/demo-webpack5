59989b48883a0fa9274b3a2450b5f862564e2d3a
FC57 <franky_57@163.com>
2024-09-25 15:42:06 +0800

    fix(types): 🐛 fix the type of webpack.devServer

--- src/types/webpack.d.ts
+++ src/types/webpack.d.ts
@@ -1,17 +1,15 @@
 // 解决 webpack.config.ts 使用 CommonJS 后类型丢失
-
-import type { DevServerOptions } from 'webpack-dev-server';
 import type { Configuration as WebpackConfiguration } from 'webpack';
 
 declare global {
   /** webpack 配置文件类型 */
-  type Configuration = WebpackConfiguration & { devServer?: DevServerOptions };
+  type Configuration = WebpackConfiguration & { devServer?: import('webpack-dev-server')['options'] };
   /** webpack 内置插件 DefinePlugin，定义变量并在打包编译时替换 */
   type WebpackDefinePlugin = (typeof import('webpack'))['DefinePlugin'];
   /** 内置模块path.resolve */
   type PathResolve = (typeof import('path'))['resolve'];
-  /** 内置模块fs.existsSync */
-  type FsExistsSync = (typeof import('fs'))['existsSync'];
+  /** 内置模块fs */
+  type NodeFs = typeof import('fs');
   /** 读取 .env 或以 .env开头文件中的变量至 process.env */
   type DotenvConfig = (typeof import('dotenv'))['config'];
   /** 自定义插件-生成打包文件列表 */
