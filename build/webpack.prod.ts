ff83150862dc5869a277a029acd010da3d5ce394
FC57 <franky_57@163.com>
2024-10-14 14:48:41 +0800

    build: 📦️ modify the prod config

--- build/webpack.prod.ts
+++ build/webpack.prod.ts
@@ -34,48 +34,50 @@ const prodConfig: Configuration = {
     ],
     chunkIds: 'named',
     runtimeChunk: 'single' // 将运行时代码提取到一个单独的 chunk 中
+
+    // 自动分包
     // splitChunks: {
-    //   chunks: "all", //initial、async和all
-    //   // minSize: 30000, // 形成一个新代码块最小的体积
-    //   // minChunks: 1, // 在分割之前，这个代码块最小应该被引用的次数
+    //   chunks: 'all', // 需要应用分包策略的 chunk，all-所有、async-默认，异步chunk、initial-普通chunk
+    //   maxSize: 60000, // 限制包的最大字节数，但可能超过预设值，因为包的基础单位是模块，模块本身超了也没办法
+    //   automaticNameDelimiter: '.', // 新chunk名称分隔符，默认 ~
+    //   minSize: 40000, // 当分包达到最低多少字节才被真正拆分，默认 30000
+    //   minChunks: 1, // 在分割之前，这个代码块最小应该被引用的次数
+
+    //   // 上述是全局配置，但分包基于缓存组
     //   cacheGroups: {
     //     vendors: {
     //       test: /[\\/]node_modules[\\/]/,
-    //       name: "vendors",
-    //       priority: 10,// 优先级，值越大，优先级越高
-    //       reuseExistingChunk: true,
+    //       name: 'vendors',
+    //       priority: 10, // 优先级，值越大，优先级越高
+    //       reuseExistingChunk: true
     //     },
     //     echarts: {
     //       test: /[\\/]node_modules[\\/]echarts|zrender(.*)/,
-    //       name: "chunk-echarts",
+    //       name: 'chunk-echarts',
     //       priority: 25,
-    //       reuseExistingChunk: true,
+    //       reuseExistingChunk: true
     //     },
     //     element: {
     //       test: /[\\/]node_modules[\\/]element(.*)/,
-    //       name: "chunk-element",
+    //       name: 'chunk-element',
     //       priority: 25,
-    //       reuseExistingChunk: true,
+    //       reuseExistingChunk: true
     //     },
     //     commons: {
-    //       name: "chunk-commons",
+    //       name: 'chunk-commons',
     //       minChunks: 2,
     //       priority: 5,
     //       minSize: 0,
-    //       reuseExistingChunk: true,
+    //       reuseExistingChunk: true
     //     },
     //     lib: {
-    //       test(module) {
-    //         return (
-    //           module.size() > 160000 &&
-    //           module.nameForCondition() &&
-    //           module.nameForCondition().includes('node_modules')
-    //         );
+    //       test(module: any) {
+    //         return module.size() > 160000 && module.nameForCondition() && module.nameForCondition().includes('node_modules');
     //       },
-    //       name: "chunk-lib",
+    //       name: 'chunk-lib',
     //       priority: 20,
-    //       minChunks:1,
-    //       reuseExistingChunk: true,
+    //       minChunks: 1,
+    //       reuseExistingChunk: true
     //     }
     //   }
     // }
